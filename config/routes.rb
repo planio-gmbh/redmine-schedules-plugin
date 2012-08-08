d9fdc6f394c985cfdba0cf732026ba113707300c
Jens Kraemer <jk@jkraemer.net>
2012-08-08 09:45:01 +0300

    fixes for 1.4, bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect 'projects/:project_id/schedules', :controller => 'schedules', :action => 'index' 
-  map.connect 'projects/:project_id/schedules/edit', :controller => 'schedules', :action => 'edit' 
-  map.connect 'projects/:project_id/schedules/report', :controller => 'schedules', :action => 'report' 
-  map.connect 'projects/:project_id/schedules/details', :controller => 'schedules', :action => 'details'
-  map.connect 'projects/:project_id/schedules/fill', :controller => 'schedules', :action => 'fill' 
-  map.connect 'account/schedule/:user_id', :controller => 'schedules', :action => 'index'
-  map.connect 'account/schedule/:user_id/edit', :controller => 'schedules', :action => 'edit'
-  map.connect 'account/schedule/:user_id/default', :controller => 'schedules', :action => 'default'
-  map.connect 'my/schedule', :controller => 'schedules', :action => 'my_index'
+  map.with_options(:controller => 'schedules') do |map|
+    map.connect 'projects/:project_id/schedules', :action => 'index' 
+    map.connect 'projects/:project_id/schedules/edit', :action => 'edit' 
+    map.connect 'projects/:project_id/schedules/report', :action => 'report' 
+    map.connect 'projects/:project_id/schedules/details', :action => 'details'
+    map.connect 'projects/:project_id/schedules/fill', :action => 'fill' 
+
+    map.connect 'schedules', :action => 'index' 
+    map.connect 'schedules/report', :action => 'report' 
+    map.connect 'schedules/users', :action => 'users' 
+    map.connect 'schedules/projects', :action => 'projects' 
+
+    map.connect 'account/schedule/:user_id', :action => 'index'
+    map.connect 'account/schedule/:user_id/edit', :action => 'edit'
+    map.connect 'account/schedule/:user_id/default', :action => 'default'
+
+    map.connect 'my/schedule', :action => 'my_index'
+  end
 end
